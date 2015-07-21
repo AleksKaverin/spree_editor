f2425e59c45ba15ca9d8db6672901a749eaa5c8c
AleksKaverin <aleks.kaverin@gmail.com>
2015-07-21 22:01:08 +0300

    add new  CKEditor

--- vendor/assets/javascripts/ckeditor/build-config.js
+++ vendor/assets/javascripts/ckeditor/build-config.js
@@ -13,10 +13,10 @@
  * (1) http://ckeditor.com/builder
  *     Visit online builder to build CKEditor from scratch.
  *
- * (2) http://ckeditor.com/builder/b01f7ea26a257bb7b7c3c7965c5c6769
+ * (2) http://ckeditor.com/builder/93ee97155c420c9e745d0b30659b7a74
  *     Visit online builder to build CKEditor, starting with the same setup as before.
  *
- * (3) http://ckeditor.com/builder/download/b01f7ea26a257bb7b7c3c7965c5c6769
+ * (3) http://ckeditor.com/builder/download/93ee97155c420c9e745d0b30659b7a74
  *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
  *
  * NOTE:
@@ -110,6 +110,7 @@ var CKBUILDER_CONFIG = {
 		'templates' : 1,
 		'toolbar' : 1,
 		'undo' : 1,
+		'widget' : 1,
 		'wsc' : 1,
 		'wysiwygarea' : 1
 	},
